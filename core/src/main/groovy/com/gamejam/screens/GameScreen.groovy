cbcac33a8afc83d745e921a67548fb9634c108d6
Chris Pound <c2.pound@gmail.com>
2013-12-13 22:38:04 -0600

    added input processor

--- core/src/main/groovy/com/gamejam/screens/GameScreen.groovy
+++ core/src/main/groovy/com/gamejam/screens/GameScreen.groovy
@@ -1,9 +1,13 @@
 package com.gamejam.screens
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.gamejam.controllers.BobController
 import com.gamejam.game.GameJam
 
 /**
@@ -12,10 +16,11 @@ import com.gamejam.game.GameJam
  * on 13-Dec-2013
  * at 8:47 PM
  */
-class GameScreen implements Screen {
+class GameScreen implements Screen, InputProcessor {
 
     final GameJam game
     private SpriteBatch batch;
+    private BobController controller;
 
     GameScreen(GameJam game) {
         this.batch = new SpriteBatch();
@@ -36,6 +41,8 @@ class GameScreen implements Screen {
 
     @Override
     void show() {
+        controller = new BobController();
+        Gdx.input.setInputProcessor(this);
         println("I'm showing!")
     }
 
@@ -65,4 +72,66 @@ class GameScreen implements Screen {
         batch.draw(bobsArea, 128, 128, 128, 128);
 
     }
+
+    @Override
+    boolean keyDown(int keycode) {
+       println("USER PRESSED: " + keycode);
+        if (keycode == Input.Keys.LEFT) {
+            controller.movingLeft();
+            println("user moving left");
+        } else if (keycode == Input.Keys.RIGHT) {
+            controller.movingRight();
+            println("user moving right");
+        }
+
+        return true;
+    }
+
+    @Override
+    boolean keyUp(int keycode) {
+        if (keycode == Input.Keys.LEFT) {
+            controller.stopMovingLeft()
+        } else if (keycode == Input.Keys.RIGHT) {
+            controller.stopMovingRight();
+        }
+
+        return true;
+    }
+
+    /*
+    UNUSED CONTROLS IGNORE
+    */
+
+    @Override
+    boolean keyTyped(char character) {
+        return false
+    }
+
+    @Override
+    boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false
+    }
+
+    @Override
+    boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false
+    }
+
+    @Override
+    boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false
+    }
+
+    @Override
+    boolean mouseMoved(int screenX, int screenY) {
+        return false
+    }
+
+    @Override
+    boolean scrolled(int amount) {
+        return false
+    }
+    /*
+    END OF UNSED CONTROLS
+     */
 }
--- core/src/main/java/com/gamejam/controllers/BobController.java
+++ core/src/main/java/com/gamejam/controllers/BobController.java
@@ -1,8 +1,62 @@
 package com.gamejam.controllers;
 
+import com.badlogic.gdx.Input;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Chris on 12/13/13.
  * Controll manager and listener for the main player Bob
-        */
+ */
 public class BobController {
+
+    private static Map<Keys, Boolean> input = new HashMap<Keys, Boolean>();
+
+    enum Keys {
+        LEFT, RIGHT
+    }
+
+    static {
+        input.put(Keys.LEFT, false);
+        input.put(Keys.RIGHT, false);
+    }
+    //esc = escape exit
+
+    //arrows handle user direction
+
+    //USER BUTTONS
+    //l-ctrl
+    //enter
+    //space
+    //l-shift
+    //z
+    //x
+
+
+    //ARCADE BUTTONS
+    //1 //2 player start and join
+    //5 //6 player coin buttons
+    public void movingLeft() {
+        input.put(Keys.LEFT, true);
+        input.put(Keys.RIGHT, false);
+    }
+
+    public void movingRight() {
+        input.put(Keys.RIGHT, true);
+        input.put(Keys.LEFT, false);
+    }
+
+    public void stopMovingRight() {
+        input.put(Keys.RIGHT, false);
+    }
+
+    public void stopMovingLeft() {
+        input.put(Keys.LEFT, false);
+    }
+
+    public void processInputMapDetermineState() {
+
+
+    }
 }
