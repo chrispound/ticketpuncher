7dba2df2a3e2b483acc77bfba08b6b3a6c7f1f81
Chris Pound <c2.pound@gmail.com>
2013-12-15 10:00:23 -0600

    evil bob init commit

---
+++ core/src/main/groovy/com/gamejam/model/EvilBob.groovy
@@ -0,0 +1,13 @@
+package com.gamejam.model
+
+import com.badlogic.gdx.graphics.Texture
+
+/**
+ * Created by Chris on 12/15/13.
+ */
+class EvilBob {
+
+    static final int lose = 5
+    Texture evilBob = new Texture("BadBob.png")
+
+}
