f545a9915f5075154a56cdd0701568c5734a8d4c
Chris Pound <c2.pound@gmail.com>
2013-12-14 20:37:04 -0600

    Added random combo generator and points

--- core/src/main/groovy/com/gamejam/model/Passenger.groovy
+++ core/src/main/groovy/com/gamejam/model/Passenger.groovy
@@ -15,11 +15,21 @@ class Passenger {
     ArrayList<Integer> combo
     Texture texture
     Vector2 position = new Vector2()
+    int delta
+    int MIN = 0
+    int MAX = 6
+
 
     Passenger(long points, ArrayList<Integer> combo, Texture texture) {
         this.points = points
-        this.combo = getPassengerCombo()
+        generatePassengerComb()
         this.texture = texture
+    }
+
+    Passenger(int delta, Texture texture) {
+        this.points = delta * 2
+        this.delta = delta
+        generatePassengerComb()
     }
 
     public ArrayList<Integer> getPassengerCombo() {
@@ -30,4 +40,22 @@ class Passenger {
         passengerCombo.add(Input.Keys.X);
         return passengerCombo;
     }
+
+    def void generatePassengerComb() {
+        if(combo == null){
+            combo = new ArrayList<Integer>();
+        }
+        def possibleKeys = [Input.Keys.SHIFT_LEFT, Input.Keys.CONTROL_LEFT, Input.Keys.Z,
+        Input.Keys.X, Input.Keys.ENTER, Input.Keys.SPACE]
+        println "random number is: " + delta
+        Random random = new Random()
+        //generate size. generate
+        for (int f = 0; f < delta; f++) {
+                 //i need to generate a random number here just
+            combo.add(possibleKeys[random.nextInt(MAX - MIN) + MIN])
+        }
+
+
+    }
+
 }
